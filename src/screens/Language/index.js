fe99f3411fc6c5b3da25bd2ec147543b106b9ec2
soyoon choi <soyoon.choi@code.berlin>
2020-10-19 17:14:08 +0200

    added terms and conditions screen

--- src/screens/Language/index.js
+++ src/screens/Language/index.js
@@ -1,37 +1,41 @@
-import React, { useState } from "react";
-import { View, Text, Button } from 'react-native'
-import { useTranslation } from 'react-i18next';
-import i18n from '../../services/i18n'
+import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
+import {useTranslation} from 'react-i18next';
+import i18n from '../../services/i18n';
 
-export default function LanguageScreen({navigation}){
-    const {t, i18n} = useTranslation();
+export default function LanguageScreen({navigation}) {
+  const {t, i18n} = useTranslation();
 
-    const [languageSelected, setLanguageSelect] = useState(false);
+  const [languageSelected, setLanguageSelect] = useState(false);
 
-    const handleLanguageSelect = () => setLanguageSelect(true); 
+  const handleLanguageSelect = () => setLanguageSelect(true);
 
-    const changeToEn = () => {
-        i18n.changeLanguage('en');
-        handleLanguageSelect();
-    }
+  const changeToEn = () => {
+    i18n.changeLanguage('en');
+    handleLanguageSelect();
+  };
 
-    const changeToDe = () => {
-        i18n.changeLanguage('de');
-        handleLanguageSelect();
-    }
+  const changeToDe = () => {
+    i18n.changeLanguage('de');
+    handleLanguageSelect();
+  };
 
-    return(
-        <View>
-            <Text>{t('common.welcome')}</Text>
-            <Text>{t('common.language')}</Text>
-            <Button title="English" onPress={changeToEn}></Button>
-            <Button title="German" onPress={changeToDe}></Button>
-            {languageSelected && 
-            <Button title="Next" onPress={() => navigation.navigate('Onboarding')}/>
-             //TODO: Add popup when user doesn't select a Language and proceed
-             //TODO: Change language of NEXT button when german is pressed 
-            }
-        </View>
-    )
-};
-
+  return (
+    <View>
+      <Text>{t('common.welcome')}</Text>
+      <Text>{t('common.language')}</Text>
+      <Button title="English" onPress={changeToEn}></Button>
+      <Button title="German" onPress={changeToDe}></Button>
+      {
+        languageSelected && (
+          <Button
+            title="Next"
+            onPress={() => navigation.navigate('TermsAndConditions')}
+          />
+        )
+        //TODO: Add popup when user doesn't select a Language and proceed
+        //TODO: Change language of NEXT button when german is pressed
+      }
+    </View>
+  );
+}
