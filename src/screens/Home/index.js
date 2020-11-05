03e915bbc3633bf31ce2f883276ca82bdb9f679a
soyoon choi <soyoon.choi@code.berlin>
2020-11-06 00:18:38 +0100

    display contacts on home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,41 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Button, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import { useTranslation } from 'react-i18next';
 
 import { Context as AuthContext } from '../../state/AuthContext';
 import appApiClient from '../../api/appApiClient';
 
+const Item = ({ item, onPress, style }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+    <Text style={styles.text}>
+      {'\n'}
+      Name:
+      {item.name}
+    </Text>
+    <Text style={styles.text}>
+      Phone Number:
+      {item.phone}
+    </Text>
+    <Text style={styles.text}>
+      Help Message:
+      {item.message}
+      {'\n'}
+    </Text>
+  </TouchableOpacity>
+);
+
 export default function Home({ navigation, route }) {
   const { state } = useContext(AuthContext);
   const { t, i18n } = useTranslation();
   const [dataSource, setDataSource] = useState([]);
+  const [selectedId, setSelectedId] = useState('');
 
   useEffect(() => {
     getContacts();
@@ -26,6 +53,17 @@ export default function Home({ navigation, route }) {
     }
   };
 
+  const renderItem = ({ item }) => {
+    const backgroundColor = item._id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    return (
+      <Item
+        item={item}
+        /* eslint no-underscore-dangle: ['error', { 'allow': ['_id'] }] */
+        onPress={() => setSelectedId(item._id)}
+        style={{ backgroundColor }}
+      />
+    );
+  };
   return (
     <View style={styles.homeView}>
       <Button
@@ -37,9 +75,18 @@ export default function Home({ navigation, route }) {
         }
       />
       <TouchableOpacity onPress={getContacts}>
-        <Text>Show contact details</Text>
+        <Text style={styles.text}>Show contact details</Text>
       </TouchableOpacity>
-      <Text>SOS button is here</Text>
+      <FlatList
+        style={styles.list}
+        data={dataSource}
+        keyExtractor={(item) => item._id}
+        extraData={selectedId}
+        enableEmptySections
+        renderItem={renderItem}
+      />
+
+      <Text style={styles.text}>SOS button is here</Text>
     </View>
   );
 }
@@ -49,5 +96,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  list: {
+    flex: 1,
+    padding: 10,
+    color: 'black',
+  },
+  text: {
+    color: 'black',
+    fontFamily: 'Courier',
   },
 });
