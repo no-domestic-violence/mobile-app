5c3d709ae3adf9921f312e361d2c2fd8c606eb07
soyoon choi <soyoon.choi@code.berlin>
2020-11-17 18:13:45 +0100

    styled buttons on language screen

--- src/screens/Language/index.js
+++ src/screens/Language/index.js
@@ -1,41 +1,52 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, Button, AsyncStorage } from 'react-native';
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 import { useTranslation } from 'react-i18next';
 import i18n from '../../services/i18n';
 import { LanguageContext } from '../../state/LanguageContext';
+import { withTheme } from 'react-native-elements';
 
 export default function LanguageScreen({ navigation }) {
   const { t, i18n } = useTranslation();
   const { availableLanguages, setAppLanguage } = useContext(LanguageContext);
 
-  const [languageSelected, setLanguageSelect] = useState(false);
-  const handleLanguageSelect = () => setLanguageSelect(true);
-
   const handleLanguageChange = async (language) => {
-    setAppLanguage(language);
-    handleLanguageSelect();
+    await setAppLanguage(language);
+    navigation.navigate('TermsAndConditions');
   };
 
   return (
-    <View>
-      <Text>{t('common.welcome')}</Text>
-      <Text>{t('common.language')}</Text>
+    <View style={styles.homePageView}>
       {availableLanguages.map((item, i) => (
         <Button
-          key={i}
           title={item.name}
-          onPress={() => handleLanguageChange(item.lang)}></Button>
+          titleStyle={{ color: '#000000' }}
+          type="solid"
+          raised
+          containerStyle={styles.button}
+          buttonStyle={{ backgroundColor: '#FFFFFF' }}
+          key={i}
+          onPress={() => handleLanguageChange(item.lang)}
+        />
       ))}
-      {
-        languageSelected && (
-          <Button
-            title="Next"
-            onPress={() => navigation.navigate('TermsAndConditions')}
-          />
-        )
-        //TODO: Add popup when user doesn't select a Language and proceed
-        //TODO: Change language of NEXT button when german is pressed
-      }
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  homePageView: {
+    backgroundColor: '#CADEEE',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: '90%',
+    height: 84,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 50,
+    margin: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
