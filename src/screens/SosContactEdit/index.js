525bcbe6bea67238f57288e135d0ac118e43b6d9
soyoon choi <soyoon.choi@code.berlin>
2020-11-22 16:25:33 +0100

    fixed icons and added translation on user settings

--- src/screens/SosContactEdit/index.js
+++ src/screens/SosContactEdit/index.js
@@ -1,6 +1,15 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {
+  faTimes,
+  faTrash,
+  faCheck,
+  faEnvelope,
+  faUser,
+  faPhone,
+} from '@fortawesome/free-solid-svg-icons';
 import { Input } from 'react-native-elements';
 import EmergencySVG from '_assets/svg/emergency.svg';
 import { useIsFocused } from '@react-navigation/native';
@@ -79,10 +88,9 @@ export default function SosContactEdit({ navigation, route }) {
       <StyledView style={styles.homeView}>
         <EmergencySVG style={styles.svg} />
         <View style={styles.container}>
-          <Icon
-            name="times"
+          <FontAwesomeIcon
+            icon={faTimes}
             size={20}
-            raised
             onPress={() => {
               navigation.navigate('SosContactHome');
             }}
@@ -91,41 +99,41 @@ export default function SosContactEdit({ navigation, route }) {
             placeholder="Name"
             value={contact.name}
             onChangeText={handleNameChange}
-            leftIcon={<Icon name="user" size={20} color="black" />}
+            leftIcon={<FontAwesomeIcon icon={faUser} size={20} color="black" />}
             leftIconContainerStyle={styles.icon}
           />
           <Input
             placeholder="Phone Number"
             value={contact.phone}
             onChangeText={handleNumberChange}
-            leftIcon={<Icon name="phone" size={20} color="black" />}
+            leftIcon={
+              <FontAwesomeIcon icon={faPhone} size={20} color="black" />
+            }
             leftIconContainerStyle={styles.icon}
           />
           <Input
             placeholder="Help Message"
             value={contact.message}
             onChangeText={handleMessageChange}
-            leftIcon={<Icon name="envelope" size={20} color="black" />}
+            leftIcon={
+              <FontAwesomeIcon icon={faEnvelope} size={20} color="black" />
+            }
             leftIconContainerStyle={styles.icon}
           />
         </View>
         <View style={styles.buttonRow}>
-          <Icon
-            name="trash"
+          <FontAwesomeIcon
+            icon={faTrash}
             size={30}
-            containerStyle={styles.iconContainer}
-            raised
             onPress={() => {
               handleRemove(contact._id, navigation);
               navigation.navigate('SosContactHome');
               // TODO: Fix navigation and api call order
             }}
           />
-          <Icon
-            name="check"
+          <FontAwesomeIcon
+            icon={faCheck}
             size={30}
-            containerStyle={styles.iconContainer}
-            raised
             onPress={() => {
               saveEdit(navigation);
               navigation.navigate('SosContactHome');
