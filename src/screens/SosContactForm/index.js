0acb334349113b474ad883b143e1207b285421a2
soyoon choi <soyoon.choi@code.berlin>
2020-11-26 17:32:59 +0100

    add next/done button to keyboards and focus to next input when these buttons are triggered, add keyboardavoidingview

--- src/screens/SosContactForm/index.js
+++ src/screens/SosContactForm/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Keyboard, KeyboardAvoidingView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
 import EmergencySVG from '_assets/svg/emergency.svg';
@@ -10,6 +10,13 @@ import appApiClient from 'api/appApiClient';
 import { Context as AuthContext } from 'state/AuthContext';
 
 export default function SosContactForm({ navigation }) {
+  const nameInputRef = React.createRef();
+  const phoneInputRef = React.createRef();
+  const messageInputRef = React.createRef();
+
+  useEffect(() => {
+    console.log(nameInputRef.current);
+  }, []);
   const { state } = useContext(AuthContext);
 
   const initialContactState = {
@@ -49,55 +56,74 @@ export default function SosContactForm({ navigation }) {
 
   return (
     <>
-      <StyledView style={styles.homeView}>
-        <EmergencySVG style={styles.svg} />
-        <View style={styles.container}>
-          <Icon
-            name="times"
-            size={20}
-            raised
-            onPress={() => {
-              navigation.navigate('SosContactHome');
-            }}
-          />
-          <Input
-            placeholder="Name"
-            value={contact.name}
-            autoCompleteType="off"
-            onChangeText={handleNameChange}
-            leftIcon={<Icon name="user" size={20} color="black" />}
-            leftIconContainerStyle={styles.icon}
-          />
-          <Input
-            placeholder="Phone Number"
-            value={contact.phone}
-            autoCompleteType="off"
-            onChangeText={handleNumberChange}
-            leftIcon={<Icon name="phone" size={20} color="black" />}
-            leftIconContainerStyle={styles.icon}
-          />
-          <Input
-            placeholder="Help Message"
-            value={contact.message}
-            autoCompleteType="off"
-            onChangeText={handleMessageChange}
-            leftIcon={<Icon name="envelope" size={20} color="black" />}
-            leftIconContainerStyle={styles.icon}
-          />
-        </View>
-        <View style={styles.buttonRow}>
-          <Icon
-            name="check"
-            size={30}
-            containerStyle={styles.iconContainer}
-            raised
-            onPress={() => {
-              saveContact(navigation);
-              navigation.navigate('SosContactHome');
-            }}
-          />
-        </View>
-      </StyledView>
+      <KeyboardAvoidingView style={{ flex: 1 }} enabled>
+        <StyledView style={styles.homeView}>
+          <EmergencySVG style={styles.svg} />
+          <View style={styles.container}>
+            <Icon
+              name="times"
+              size={20}
+              raised
+              onPress={() => {
+                navigation.navigate('SosContactHome');
+              }}
+            />
+            <Input
+              placeholder="Name"
+              ref={nameInputRef}
+              value={contact.name}
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                phoneInputRef.current && phoneInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+              autoCompleteType="off"
+              onChangeText={handleNameChange}
+              leftIcon={<Icon name="user" size={20} color="black" />}
+              leftIconContainerStyle={styles.icon}
+            />
+            <Input
+              placeholder="Phone Number"
+              ref={phoneInputRef}
+              keyboardType="numeric"
+              returnKeyType="done"
+              onSubmitEditing={() =>
+                messageInputRef.current && messageInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+              value={contact.phone}
+              autoCompleteType="off"
+              onChangeText={handleNumberChange}
+              leftIcon={<Icon name="phone" size={20} color="black" />}
+              leftIconContainerStyle={styles.icon}
+            />
+            <Input
+              placeholder="Help Message"
+              ref={messageInputRef}
+              value={contact.message}
+              returnKeyType="done"
+              autoCompleteType="off"
+              blurOnSubmit={false}
+              onChangeText={handleMessageChange}
+              onSubmitEditing={Keyboard.dismiss}
+              leftIcon={<Icon name="envelope" size={20} color="black" />}
+              leftIconContainerStyle={styles.icon}
+            />
+          </View>
+          <View style={styles.buttonRow}>
+            <Icon
+              name="check"
+              size={30}
+              containerStyle={styles.iconContainer}
+              raised
+              onPress={() => {
+                saveContact(navigation);
+                navigation.navigate('SosContactHome');
+              }}
+            />
+          </View>
+        </StyledView>
+      </KeyboardAvoidingView>
     </>
   );
 }
