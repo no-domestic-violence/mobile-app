4ba919a2b5ca3c684d29541bae313267afa62a74
Irina Baeva <irina.baeva@bonial.com>
2020-12-05 14:14:53 +0100

    fix units

--- src/styles/shared/StyledHotlinesListItem.js
+++ src/styles/shared/StyledHotlinesListItem.js
@@ -2,11 +2,11 @@ import { Colors } from '../index';
 import styled from 'styled-components/native'
 
 export const StyledHotlinesListItem = styled.TouchableOpacity`
-    paddingVertical: 20;
-    paddingHorizontal: 20;
+    paddingVertical: 20px;
+    paddingHorizontal: 20px;
     flexShrink: 1;
     flexDirection: row;
     justifyContent: space-between;
     backgroundColor: ${Colors.secondary};
-    borderRadius: 30;
+    borderRadius: 30px;
 `;
