ab9495a8428e46b4355a36e59e83cd40263278a3
soyoon choi <soyoon.choi@code.berlin>
2020-12-11 19:53:08 +0100

    mock svg

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: 'jest-expo'
+  preset: 'jest-expo',
+  transform: {
+    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js',
+    '^.+\\.svg$': '<rootDir>/svgTransform.js',
+  },
 };
