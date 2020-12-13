5d2a243f1f47e8fb6172478e4483a23147d72eca
soyoon choi <soyoon.choi@code.berlin>
2020-12-13 02:23:04 +0100

    add test on terms and conditions button

--- src/screens/TermsAndConditions/TermsAndConditions.test.js
+++ src/screens/TermsAndConditions/TermsAndConditions.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import TermsAndConditionsScreen from './index';
 
 jest.mock('react-i18next');
@@ -8,5 +8,12 @@ describe('<TermsAndConditionsScreen />', () => {
   it('should match snapshot', () => {
     const result = render(<TermsAndConditionsScreen />).toJSON();
     expect(result).toMatchSnapshot();
+  });
+  it('should be not tappable when disabled', () => {
+    const onPress = jest.fn();
+    const tree = render(<TermsAndConditionsScreen />);
+    const button = tree.getByTestId('terms-conditions-button');
+    fireEvent.press(button);
+    expect(onPress).not.toHaveBeenCalled();
   });
 });
--- src/screens/TermsAndConditions/index.js
+++ src/screens/TermsAndConditions/index.js
@@ -34,6 +34,7 @@ export default function TermsAndConditionsScreen({ navigation }) {
         <TermsAndConditionsText />
       </ScrollView>
       <StyledButton
+        testID="terms-conditions-button"
         disabled={!isRead}
         onPress={() => navigation.navigate('Onboarding')}
         style={!isRead ? styles.buttonDisabled : styles.button}>
