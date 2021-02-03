d986d1a8f6a417c8431b13cbf80df0a0806a9489
Irina Baeva <irina.baeva@bonial.com>
2021-02-03 17:49:54 +0100

    Fix styled components and add format rules

--- src/styles/shared/StyledInputAuth.js
+++ src/styles/shared/StyledInputAuth.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
 export const StyledInputAuth = styled.TextInput`
-    width: 85%;
-    backgroundColor: #fff;
-    color: #000;
-    borderRadius: 5px;
-    fontSize: 18px;
-    fontWeight: 400;
-    paddingLeft: 2%;
-    paddingTop: 4%;
-    paddingBottom: 4%;
+  width: 85%;
+  background-color: #fff;
+  color: #000;
+  border-radius: 5px;
+  font-size: 18px;
+  font-weight: 400;
+  padding-left: 2%;
+  padding-top: 4%;
+  padding-bottom: 4%;
 `;
--- src/styles/shared/StyledView.js
+++ src/styles/shared/StyledView.js
@@ -1,8 +1,8 @@
 import { Colors } from '../index';
-import styled from 'styled-components/native'
+import styled from 'styled-components/native';
 
 export const StyledView = styled.View`
   background-color: ${Colors.primary};
   flex: 1;
-  alignItems: center;
-`
+  align-items: center;
+`;
