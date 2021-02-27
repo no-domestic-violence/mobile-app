58b305bc2d5d0dc55a808bd9b93b35351c87e8cb
Irina Baeva <irina.baeva@bonial.com>
2021-02-27 16:18:15 +0100

    Add test for userInfo component

---
+++ src/components/user-info/UserInfo.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import UserInfo from './index';
+
+describe('User information component', () => {
+  const username = 'Celeste';
+  it('renders name of user from props', () => {
+    const { queryByText } = render(<UserInfo username={username} />);
+    expect(queryByText(username)).not.toBeNull();
+  });
+});
