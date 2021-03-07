78d59b1321c70ae9593ee51a0cf85ce9ace790b3
Irina Baeva <irina.baeva@bonial.com>
2021-03-07 16:29:08 +0100

    Initial test of hotlines api request

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,59 +1,35 @@
-import axios from './appApiClient';
-import { getHotlinesData } from './index';
-
-jest.mock('./appApiClient', () => {
-  const hotlinesDataMock = [
-    {
-      _id: '5f9db611c7cc881787ba620e',
-      city: 'Hamburg',
-      country: 'Germany',
-      organisation_name: "Nelson's Horsenettle",
-      phone: '+49 543 513 8358',
-    },
-    {
-      _id: '5f9db611c7cc881787ba620a',
-      city: 'Hamburg',
-      country: 'Germany',
-      organisation_name: 'Test name',
-      phone: '+49 543 510 8358',
-    },
-    {
-      _id: '5f9db611c7cc881787ba620b',
-      city: 'Hamburg',
-      country: 'Germany',
-      organisation_name: 'Test name 2',
-      phone: '+49 543 514 8358',
-    },
-  ];
-  return {
-    baseURL: 'https://pool-api-mobile.herokuapp.com/api',
-    get: jest.fn().mockResolvedValue({
-      data: hotlinesDataMock,
-    }),
+const hotlinesDataMock = [
+  {
+    _id: '5f9db611c7cc881787ba620e',
+    city: 'Hamburg',
+    country: 'Germany',
+    organisation_name: "Nelson's Horsenettle",
+    phone: '+49 543 513 8358',
+  },
+  {
+    _id: '5f9db611c7cc881787ba620a',
+    city: 'Hamburg',
+    country: 'Germany',
+    organisation_name: 'Test name',
+    phone: '+49 543 510 8358',
+  },
+  {
+    _id: '5f9db611c7cc881787ba620b',
+    city: 'Hamburg',
+    country: 'Germany',
+    organisation_name: 'Test name 2',
+    phone: '+49 543 514 8358',
+  },
+];
+describe('appApiClient', () => {
+  const appApiClient = {
+    getHotlinesData: jest.fn(() => Promise.resolve({ data: hotlinesDataMock }))
   };
-});
-describe('getHotlinesData by search parameter', () => {
-  afterEach(() => jest.resetAllMocks());
-  it('fetches hotlines by search param', async () => {
+ it('get hotlines data by search param', async() => {
     const search = 'hamburg';
-    const hotlinesData = await getHotlinesData(search);
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith('/hotlines', {
-      params: { searchTerm: search },
-    });
+    const hotlinesData = await appApiClient.getHotlinesData(search);
+    expect(appApiClient.getHotlinesData).toHaveBeenCalledTimes(1);
+    expect(appApiClient.getHotlinesData).toHaveBeenCalledWith(search);
     expect(hotlinesData.data.length).toEqual(3);
-  });
-
-  it('resolves with error', async () => {
-    const serverError = new Error('Some network error');
-    axios.get.mockRejectedValueOnce(Promise.reject(serverError));
-    console = { error: jest.fn() };
-
-    try {
-      await getHotlinesData('?');
-    } catch (err) {
-      expect(console.error).toBeCalled();
-      expect(err).toEqual(serverError);
-    }
-  });
+ });
 });
