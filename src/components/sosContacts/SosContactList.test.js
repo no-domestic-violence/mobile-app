4a23c84066d05b2cf857d40b3ff2d795515fc562
soyoon choi <soyoon.choi@code.berlin>
2021-03-09 22:54:07 +0100

    add unit test on sos contact list

--- src/components/sosContacts/SosContactList.test.js
+++ src/components/sosContacts/SosContactList.test.js
@@ -11,4 +11,9 @@ describe('<SosContactList />', () => {
     const result = render(<SosContactList />).toJSON();
     expect(result).toMatchSnapshot();
   });
+  it('should have both of two contact list elements', () => {
+    const { getByTestId } = render(<SosContactList />);
+    expect(getByTestId('first-contact')).not.toBeNull();
+    expect(getByTestId('second-contact')).not.toBeNull();
+  });
 });
