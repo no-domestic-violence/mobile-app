cf06647f12d6aced79c64868006f73e9d4283c3c
soyoon choi <soyoon.choi@code.berlin>
2021-03-12 19:14:07 +0100

    change deletecontact to send response.data.id

--- src/state/SosContext.js
+++ src/state/SosContext.js
@@ -1,6 +1,6 @@
 /* eslint no-underscore-dangle: ['error', { 'allow': ['_id'] }] */
 import * as SecureStore from 'expo-secure-store';
-import { apiInstance } from 'api/';
+import appApiClient, { apiInstance } from 'api/';
 import createAppContext from './CreateAppContext';
 
 const ACTIONS = {
@@ -27,7 +27,7 @@ const sosReducer = (state, action) => {
     case ACTIONS.DELETE_CONTACT:
       return {
         ...contacts,
-        contacts: contacts.filter((contact) => contact._id !== id),
+        contacts: contacts.filter((contact) => contact._id !== action.payload),
       };
     case ACTIONS.GET_CONTACTS:
       return { contacts: action.payload };
@@ -40,9 +40,11 @@ const getContacts = (dispatch) => async () => {
   try {
     const username = await SecureStore.getItemAsync('username');
     const token = await SecureStore.getItemAsync('token');
-    const response = await apiInstance.get(`/users/${username}/contacts`, {
-      headers: { 'auth-token': token },
-    });
+    const response = await appApiClient.getSosContacts(username, token);
+    // apiInstance.get(`/users/${username}/contacts`, {
+    //   headers: { 'auth-token': token },
+    // });
+    // console.log(response);
     dispatch({ type: ACTIONS.GET_CONTACTS, payload: response.data.contacts });
   } catch (error) {
     console.error(error);
@@ -52,32 +54,32 @@ const getContacts = (dispatch) => async () => {
 const deleteContact = (dispatch) => async ({ id }) => {
   const username = await SecureStore.getItemAsync('username');
   const token = await SecureStore.getItemAsync('token');
-  await apiInstance
-    .delete(`/users/${username}/contacts/`, {
-      params: { id },
-      headers: { 'auth-token': token },
-    })
-    .then(() => {
-      dispatch({ type: ACTIONS.DELETE_CONTACT, payload: { id } });
-    })
-    .catch((e) => {
-      alert(e);
-    });
+  try {
+    const response = await appApiClient.deleteSosContact(username, id, token);
+    dispatch({ type: ACTIONS.DELETE_CONTACT, payload: response.data.id });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const addContact = (dispatch) => async (data) => {
   const username = await SecureStore.getItemAsync('username');
   const token = await SecureStore.getItemAsync('token');
-  await apiInstance
-    .patch(`/users/${username}/contacts/`, data, {
-      headers: { 'auth-token': token },
-    })
-    .then(() => {
-      dispatch({ type: ACTIONS.ADD_CONTACT, payload: { data } });
-    })
-    .catch((e) => {
-      alert(e);
-    });
+
+  try {
+    const response = await appApiClient.addSosContact(username, data, token);
+    // console.log('response', response);
+    // console.log(response.data);
+    // console.log('data', data);
+    // await apiInstance
+    //   .patch(`/users/${username}/contacts/`, data, {
+    //     headers: { 'auth-token': token },
+    //   })
+
+    dispatch({ type: ACTIONS.ADD_CONTACT, payload: { data } });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const editContact = (dispatch) => async ({ data, id }) => {
