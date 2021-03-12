7990157a30cc67f93680a27ff0a8735979774a8a
soyoon choi <soyoon.choi@code.berlin>
2021-03-13 00:47:31 +0100

    fix wrong import after rename

--- src/components/sosContactList/SosContactList.test.js
+++ src/components/sosContactList/SosContactList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import SosContactList from './SosContactList';
+import SosContactList from '.';
 
 jest.mock('@fortawesome/react-native-fontawesome', () => ({
   FontAwesomeIcon: '',
