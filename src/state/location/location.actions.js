d47bce44533fd3bed55cc143282d7fcbc5233fc7
Irina Baeva <irina.baeva@bonial.com>
2021-03-28 10:37:57 +0200

    [Refactoring] fix divergent string change for auth context

--- src/state/location/location.actions.js
+++ src/state/location/location.actions.js
@@ -1,17 +1,18 @@
 import appApiClient from 'api/';
+import * as types from './location.types';
 
 const updateCurrentLocation = (dispatch) => (location) => {
-  dispatch({ type: 'UPDATE_LOCATION', payload: location });
+  dispatch({ type: types.UPDATE_LOCATION, payload: location });
 };
 
 const fetchShelters = (dispatch) => async () => {
   const response = await appApiClient.getSheltersData();
-  dispatch({ type: 'FETCH_SHELTERS', payload: response.data });
+  dispatch({ type: types.FETCH_SHELTERS, payload: response.data });
 };
 
 const searchHotlinesByParam = (dispatch) => async (searchParam) => {
   const response = await appApiClient.getHotlinesData(searchParam);
-  dispatch({ type: 'SEARCH_HOTLINES', payload: response.data });
+  dispatch({ type: types.SEARCH_HOTLINES, payload: response.data });
 };
 
 export { updateCurrentLocation, fetchShelters, searchHotlinesByParam };
--- src/state/location/location.reducer.js
+++ src/state/location/location.reducer.js
@@ -1,10 +1,16 @@
+import {
+  UPDATE_LOCATION,
+  FETCH_SHELTERS,
+  SEARCH_HOTLINES,
+} from './location.types';
+
 const locationReducer = (state, action) => {
   switch (action.type) {
-    case 'UPDATE_LOCATION':
+    case UPDATE_LOCATION:
       return { ...state, currentLocation: action.payload };
-    case 'FETCH_SHELTERS':
+    case FETCH_SHELTERS:
       return { ...state, shelters_list: action.payload };
-    case 'SEARCH_HOTLINES':
+    case SEARCH_HOTLINES:
       return { ...state, hotlinesData: action.payload };
     default:
       return state;
