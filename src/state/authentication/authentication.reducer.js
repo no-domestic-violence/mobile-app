9af8e9179a3ce1bb2c1b54640388e83b56136bb9
Irina Baeva <irina.baeva@bonial.com>
2021-03-28 09:12:29 +0200

    [Refactoring] fix divergent string change for auth context

--- src/state/authentication/authentication.reducer.js
+++ src/state/authentication/authentication.reducer.js
@@ -1,57 +1,78 @@
+import {
+  SIGNUP_ERROR,
+  LOGIN_ERROR,
+  SIGNUP_SUCCESS,
+  LOGIN_SUCCESS,
+  CHANGE_PASSWORD_SUCCESS,
+  CHANGE_PASSWORD_ERROR,
+  AUTH_SUCCESS,
+  LOGOUT,
+  DELETE_ACCOUNT_SUCCESS,
+  DELETE_ACCOUNT_ERROR,
+  REMOVE_ERRORS,
+  REMOVE_MESSAGES,
+  CHECK_FIRST_LAUNCH,
+} from './authentication.types';
+
 const authReducer = (state, { type, payload }) => {
   switch (type) {
-    case 'SIGNUP_ERROR':
-    case 'LOGIN_ERROR':
+    case SIGNUP_ERROR:
+    case LOGIN_ERROR:
       return {
         ...state,
         errorMessage: payload,
       };
-    case 'SIGNUP_SUCCESS':
-    case 'LOGIN_SUCCESS':
+    case SIGNUP_SUCCESS:
+    case LOGIN_SUCCESS:
       return {
         ...state,
         token: payload.token,
         username: payload.user.username,
         errorMessage: '',
       };
-    case 'CHANGE_PASSWORD_SUCCESS':
+    case CHANGE_PASSWORD_SUCCESS:
       return {
         ...state,
         successMessage: payload,
       };
-    case 'CHANGE_PASSWORD_ERROR':
+    case CHANGE_PASSWORD_ERROR:
       return {
         ...state,
         errorMessage: payload,
       };
-    case 'AUTH_SUCCESS':
+    case AUTH_SUCCESS:
       return {
         ...state,
         token: payload.token,
         username: payload.username,
       };
-    case 'LOGOUT':
+    case LOGOUT:
       return {
         token: '',
         username: '',
       };
-    case 'DELETE_ACCOUNT':
+    case DELETE_ACCOUNT_SUCCESS:
       return {
         token: '',
         username: '',
         successMessage: payload.data,
       };
-    case 'REMOVE_ERRORS':
+    case DELETE_ACCOUNT_ERROR:
+      return {
+        ...state,
+        errorMessage: payload,
+      };
+    case REMOVE_ERRORS:
       return {
         ...state,
         errorMessage: '',
       };
-    case 'REMOVE_MESSAGES':
+    case REMOVE_MESSAGES:
       return {
         ...state,
         successMessage: '',
       };
-    case 'CHECK_FIRST_LAUNCH':
+    case CHECK_FIRST_LAUNCH:
       return {
         isFirstLaunch: payload,
       };
