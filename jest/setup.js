fa954dbecff7ec71c9dfdedd250225f9204d66e8
soyoon choi <soyoon.choi@code.berlin>
2021-04-25 22:08:54 +0200

    mock i18next

--- jest/setup.js
+++ jest/setup.js
@@ -5,7 +5,6 @@ jest.mock('@fortawesome/react-native-fontawesome', () => ({
   FontAwesomeIcon: '',
 }));
 
-
 jest.mock('react-native-reanimated', () => {
   // eslint-disable-next-line
   const Reanimated = require('react-native-reanimated/mock');
@@ -20,3 +19,16 @@ jest.mock('react-native-reanimated', () => {
 // Silence the warning: Animated: `useNativeDriver` is not supported because the
 //  native animated module is missing
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
+jest.mock('i18next', () => ({
+  use: () => {
+    return {
+      use: () => {
+        return {
+          init: () => {},
+        };
+      },
+    };
+  },
+  t: (key) => key,
+}));
