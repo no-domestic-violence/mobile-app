26dfc54c34915fa951fda6b5869c26d9b3e786c3
Irina Baeva <irina.baeva@bonial.com>
2021-04-30 14:46:39 +0200

    [Refactoring auth reducer] remove duplicated code and repeated swutches smell with functional decomposition

--- src/state/authentication/authentication.reducer.js
+++ src/state/authentication/authentication.reducer.js
@@ -1,62 +1,92 @@
-import types from './authentication.types'
+import types from './authentication.types';
 
-const initialState = {
+const initialAuthState = {
   token: '',
   username: '',
 };
 
-const authReducer = (state, { type, payload }) => {
-  switch (type) {
-    case types.AUTH_ERROR:
-      return {
-        ...state,
-        errorMessage: payload,
-      };
-    case types.LOGIN_SIGNUP_SUCCESS:
-      return {
-        ...state,
-        token: payload.token,
-        username: payload.user.username,
-        errorMessage: '',
-      };
-    case types.CHANGE_PASSWORD_SUCCESS:
-      return {
-        ...state,
-        successMessage: payload,
-      };
-    case types.AUTH_SUCCESS:
-      return {
-        ...state,
-        token: payload.token,
-        username: payload.username,
-      };
-    case types.LOGOUT:
-      return {
-        initialState,
-      };
-    case types.DELETE_ACCOUNT_SUCCESS:
-      return {
-        ...initialState,
-        successMessage: payload.data,
-      };
-    case types.REMOVE_ERRORS:
-      return {
-        ...state,
-        errorMessage: '',
-      };
-    case types.REMOVE_MESSAGES:
-      return {
-        ...state,
-        successMessage: '',
-      };
-    case types.CHECK_FIRST_LAUNCH:
-      return {
-        ...state,
-        isFirstLaunch: payload,
-      };
-    default:
-      return state;
-  }
+const getAuthErrorCase = (state, { payload }) => {
+  return { ...state, errorMessage: payload };
 };
+
+const loginSignupSuccessCase = (state, { payload }) => {
+  return {
+    ...state,
+    token: payload.token,
+    username: payload.user.username,
+    errorMessage: '',
+  };
+};
+
+const changePasswordSuccessCase = (state, { payload }) => {
+  return {
+    ...state,
+    successMessage: payload,
+  };
+};
+
+const authSuccessCase = (state, { payload }) => {
+  return {
+    ...state,
+    token: payload.token,
+    username: payload.username,
+  };
+};
+
+const logoutCase = (state = initialAuthState, { payload }) => {
+  return {
+    state,
+  };
+};
+
+const deleteAccountSuccessCase = (state = initialAuthState, { payload }) => {
+  return {
+    state,
+    successMessage: payload.data,
+  };
+};
+
+const removeErrorsCase = (state, { payload }) => {
+  return {
+    ...state,
+    errorMessage: '',
+  };
+};
+
+const removeMessagesCase = (state, { payload }) => {
+  return {
+    ...state,
+    successMessage: '',
+  };
+};
+
+const checkFirstLaunchCase = (state, { payload }) => {
+  return {
+    ...state,
+    isFirstLaunch: payload,
+  };
+};
+
+function createReducer(handlers) {
+  return function reducer(state, action) {
+    if (handlers.hasOwnProperty(action.type)) {
+      return handlers[action.type](state, action);
+    } else {
+      return state;
+    }
+  };
+}
+
+const authReducer = createReducer({
+  [types.AUTH_ERROR]: getAuthErrorCase,
+  [types.LOGIN_SIGNUP_SUCCESS]: loginSignupSuccessCase,
+  [types.CHANGE_PASSWORD_SUCCESS]: changePasswordSuccessCase,
+  [types.AUTH_SUCCESS]: authSuccessCase,
+  [types.LOGOUT]: logoutCase,
+  [types.DELETE_ACCOUNT_SUCCESS]: deleteAccountSuccessCase,
+  [types.REMOVE_ERRORS]: removeErrorsCase,
+  [types.REMOVE_MESSAGES]: removeMessagesCase,
+  [types.CHECK_FIRST_LAUNCH]: checkFirstLaunchCase,
+});
 
 export default authReducer;
