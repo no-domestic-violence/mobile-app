cfab93b1ebea85eff69bf3d52056c9495d095217
Soyoon Choi <soyoon.choi@code.berlin>
2021-05-14 11:11:54 +0200

    add tests for terms and conditions screen

--- src/screens/TermsAndConditions/TermsAndConditions.test.js
+++ src/screens/TermsAndConditions/TermsAndConditions.test.js
@@ -3,17 +3,38 @@ import { fireEvent, render } from '@testing-library/react-native';
 import TermsAndConditionsScreen from './index';
 
 jest.mock('react-i18next');
-
+const mockNavigation = {
+  navigate: jest.fn()
+}
 describe('<TermsAndConditionsScreen />', () => {
   it('should match snapshot', () => {
     const result = render(<TermsAndConditionsScreen />).toJSON();
     expect(result).toMatchSnapshot();
   });
-  it('should be not tappable when disabled', () => {
+  test('button should be not tappable by default', () => {
     const onPress = jest.fn();
     const tree = render(<TermsAndConditionsScreen />);
     const button = tree.getByTestId('terms-conditions-button');
     fireEvent.press(button);
     expect(onPress).not.toHaveBeenCalled();
   });
+  test('button should be tappable when scrolled down near to the bottom', () => {
+    const eventData = {
+      nativeEvent: {
+        layoutMeasurement: {
+          height: 0
+        },
+        contentOffset: {
+          y: 200
+        },
+        contentSize: {
+          height: 80
+        }
+      }
+    }
+    const { getByTestId } = render(<TermsAndConditionsScreen navigation={mockNavigation}/>);
+    fireEvent.scroll(getByTestId('terms-conditions-scroll-view'), eventData);
+    fireEvent.press(getByTestId('terms-conditions-button'));
+    expect(mockNavigation.navigate).toHaveBeenCalledWith('Onboarding');
+  })
 });
