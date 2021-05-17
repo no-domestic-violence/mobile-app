c32b3858512f0ea4d533aaff0aeeca5f58354edb
Irina Baeva <irina.baeva@bonial.com>
2021-05-17 14:36:48 +0200

    Fix auth form test

--- src/components/authentication-form/AuthForm.test.js
+++ src/components/authentication-form/AuthForm.test.js
@@ -6,7 +6,7 @@ const onSubmitForm = jest.fn();
 describe('Authentication form component', () => {
   it('should render the right header and username, email, password for sign up', () => {
     const formType = 'sign up';
-    const { queryByPlaceholderText, queryByText, queryByTestId } = render(
+    const { getByPlaceholderText, getByText, getByTestId } = render(
       <AuthForm
         formType={formType}
         headerForm='Sign up'
@@ -14,15 +14,15 @@ describe('Authentication form component', () => {
         buttonText='Sign up'
       />
     );
-    expect(queryByTestId('form-header')).toHaveTextContent('Sign up');
-    expect(queryByPlaceholderText(/username/i)).not.toBeNull();
-    expect(queryByPlaceholderText(/email/i)).not.toBeNull();
-    expect(queryByPlaceholderText(/password/i)).not.toBeNull();
-    expect(queryByText('Sign up')).not.toBeNull();
+    expect(getByTestId('form-header')).toHaveTextContent('Sign up');
+    expect(getByPlaceholderText(/username/i)).not.toBeNull();
+    expect(getByPlaceholderText(/email/i)).not.toBeNull();
+    expect(getByPlaceholderText(/password/i)).not.toBeNull();
+    expect(getByText('Sign up')).not.toBeNull();
   });
 
   it('should render the right header and email, password for log in', () => {
-    const { queryByPlaceholderText, queryByTestId } = render(
+    const { getByPlaceholderText, getByTestId } = render(
       <AuthForm
         formType='log in'
         headerForm='Log in'
@@ -30,13 +30,13 @@ describe('Authentication form component', () => {
         buttonText='log in'
       />
     );
-    expect(queryByTestId('form-header')).toHaveTextContent('Log in');
-    expect(queryByPlaceholderText(/email/i)).not.toBeNull();
-    expect(queryByPlaceholderText(/password/i)).not.toBeNull();
+    expect(getByTestId('form-header')).toHaveTextContent('Log in');
+    expect(getByPlaceholderText(/email/i)).not.toBeNull();
+    expect(getByPlaceholderText(/password/i)).not.toBeNull();
   });
 
   it('should render the right header and email, password, old password for change password form', () => {
-    const { queryByPlaceholderText, queryByText, queryByTestId } = render(
+    const { getByPlaceholderText, getByText, getByTestId } = render(
       <AuthForm
         formType='change password'
         headerForm='Change password'
@@ -44,11 +44,11 @@ describe('Authentication form component', () => {
         buttonText='confirm'
       />
     );
-    expect(queryByTestId('form-header')).toHaveTextContent('Change password');
-    expect(queryByPlaceholderText(/email/i)).not.toBeNull();
-    expect(queryByPlaceholderText(/old password/i)).not.toBeNull();
-    expect(queryByPlaceholderText('Password')).not.toBeNull();
-    expect(queryByText(/confirm/i)).not.toBeNull();
+    expect(getByTestId('form-header')).toHaveTextContent('Change password');
+    expect(getByPlaceholderText(/email/i)).not.toBeNull();
+    expect(getByPlaceholderText(/old password/i)).not.toBeNull();
+    expect(getByPlaceholderText('Password')).not.toBeNull();
+    expect(getByText(/confirm/i)).not.toBeNull();
   });
 
   describe('should match snapshot', () => {
