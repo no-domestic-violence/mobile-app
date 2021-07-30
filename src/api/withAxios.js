cfdef5b734d312eb130f95bb57f98a2ca0910c9a
Soyoon Choi <soyoon.choi@code.berlin>
2021-07-30 16:04:03 +0200

    intercept 403 responses to refresh token and logout user on expiration

---
+++ src/api/withAxios.js
@@ -0,0 +1,62 @@
+import { useContext, useMemo } from 'react';
+import axios from 'axios';
+import { AuthContext } from 'state/';
+import { getTokenSecureStorage, getRefreshTokenSecureStorage } from 'helpers';
+import * as SecureStore from 'expo-secure-store';
+import apiInstance from './apiInstance';
+import appApiClient from './index';
+
+const WithAxios = ({ children }) => {
+  const { signout } = useContext(AuthContext);
+
+  useMemo(() => {
+    apiInstance.interceptors.request.use(
+      async (config) => {
+        const token = await getTokenSecureStorage();
+        if (token) {
+          config.headers.authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      async (error) => Promise.reject(error)
+    );
+
+    apiInstance.interceptors.response.use(
+      // return successful response
+      (response) => {
+        return response;
+      },
+      async (error) => {
+        const originalRequest = error.config;
+        const refreshToken = await getRefreshTokenSecureStorage();
+        // logout user if refresh token did not work (expired)
+        if (originalRequest.url === '/refreshToken') {
+            signout();
+            return appApiClient.logoutUser(refreshToken);
+        }
+        // try request with new access token when access token expired
+        if (
+          refreshToken &&
+          error.response.status === 403 &&
+          !originalRequest._retry
+        ) {
+          originalRequest._retry = true;
+          return apiInstance
+            .post('/refreshToken', { refreshToken: refreshToken })
+            .then((res) => {
+              if (res.status === 201) {
+                const token = res.data.accessToken;
+                SecureStore.setItemAsync('token', token);
+                originalRequest.headers.authorization = `Bearer ${token}`;
+                return axios(originalRequest);
+              }
+            });
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [signout]);
+  return children;
+};
+
+export default WithAxios;
